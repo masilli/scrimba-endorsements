acd1e34806999a6655b11b6de4d01d2b873e624a
masilli <masilli@gmail.com>
2024-05-17 01:15:22 +0100

    finished project

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://endorsements-db-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -19,7 +19,8 @@ const receiverName = document.getElementById("receiver-name")
 const publishButton = document.getElementById("publish-button")
 const messageBoard = document.getElementById("msg-board")
 
-publishButton.addEventListener("click", function() {
+publishButton.addEventListener("click", function(e) {
+    e.preventDefault()
     publishMessage()
 })
 
@@ -28,58 +29,50 @@ function publishMessage() {
     let senderValue = senderName.value
     let receiverValue = receiverName.value
     const completeMsg = {
-        textMsg: textMsg,
+        message: textMsg,
         sender: senderValue,
-        receiver: receiverValue
+        receiver: receiverValue,
+        likes: 0
     }
     
-    if (textMsg !== "") {
-        push(messagesInDB, completeMsg);
+    if (textMsg.trim() === "" || senderValue.trim() === "" || receiverValue.trim() === "") {
+        textArea.style.outline = "2px solid red"
+        senderName.style.outline = "2px solid red"
+        receiverName.style.outline = "2px solid red"
+        setTimeout(function() {
+            textArea.style.outline = "none"
+            senderName.style.outline = "none"
+            receiverName.style.outline = "none"
+        }, 300)
+        setTimeout(function() {
+            textArea.style.outline = "2px solid red"
+            senderName.style.outline = "2px solid red"
+            receiverName.style.outline = "2px solid red"
+        }, 600)
+        setTimeout(function() {
+            textArea.style.outline = "none"
+            senderName.style.outline = "none"
+            receiverName.style.outline = "none"
+        }, 900)
+    } else {
+        push(messagesInDB, completeMsg)
         clearTextArea()
         freddie.classList.add("publish")
         setTimeout(function() {
             freddie.classList.remove("publish")
-        }, 3000)
-    } else {
-        textArea.value = ">> add message here"
-        setTimeout(function() {
-            textArea.value = ""
-        }, 500)
+        }, 2000)
     }
 }
 
-// onValue(messagesInDB, function(snapshot) {
-//     if (snapshot.exists()) {
-//         let itemsArray = Object.entries(snapshot.val())
-    
-//         clearMessages()
-        
-//         for (let i = 0; i < itemsArray.length; i++) {
-//             let currentItem = itemsArray[i]
-//             let currentItemID = currentItem[0]
-//             let currentItemValue = currentItem[1]
-            
-//             appendMessageToBoard(currentItem)
-//         }    
-//     } else {
-//         messageBoard.innerHTML = "Nothing to see here..."
-//     }
-// })
-
 onValue(messagesInDB, function(snapshot) {
     if (snapshot.exists()) {
-
+        let endorsementsArray = Object.entries(snapshot.val())
+    
         clearMessages()
         
-        snapshot.forEach((childSnapshot) => {
-
-            const data = childSnapshot.val()
-            const textMsg = data.textMsg
-            const sender = data.sender
-            const receiver = data.receiver
-
-            appendMessageToBoard(textMsg, sender, receiver)
-        })
+        for (let i = endorsementsArray.length - 1; i >= 0; i--) {
+            appendMessageToBoard(endorsementsArray[i])
+        }    
     } else {
         messageBoard.innerHTML = "Nothing to see here..."
     }
@@ -91,26 +84,62 @@ function clearMessages() {
 
 function clearTextArea() {
     textArea.value = ""
+    senderName.value = ""
+    receiverName.value = ""
 }
 
-// function appendMessageToBoard(item) {
-//     let itemID = item[0]
-//     let itemValue = item[1]
-    
-//     let newEl = document.createElement("div")
-//     newEl.setAttribute("tabindex", "0")
-//     newEl.classList.add("message")
-    
-//     newEl.textContent = itemValue
-    
-//     messageBoard.append(newEl)
-// }
+function appendMessageToBoard(endorsement) {
 
-function appendMessageToBoard(textMsg, sender, receiver) {
-    let newEl = document.createElement("div")
-    newEl.classList.add("message")
+    let endorsementID = endorsement[0]
+    let endorsementFrom = endorsement[1].sender
+    let endorsementTo = endorsement[1].receiver
+    let endorsementMsg = endorsement[1].message
+    let endorsementLikes = endorsement[1].likes
+
+    let newMessage = document.createElement("div")
+    newMessage.id = endorsementID
+
+    newMessage.classList.add("endorsement")
+
+    let msgHeader = document.createElement("div")
+    msgHeader.classList.add("msg-header")
+    msgHeader.innerHTML = `
+        <div>from <span class="sender">${endorsementFrom}</span></div>
+        <div>to <span class="receiver">${endorsementTo}</span></div>
+    `
+    newMessage.appendChild(msgHeader)
+
+    let messageContent = document.createElement("div")
+    messageContent.classList.add("message")
+    messageContent.textContent = endorsementMsg
+    newMessage.appendChild(messageContent)
+
+    let msgFooter = document.createElement("div")
+    msgFooter.classList.add("msg-footer")
+
+    let likeButton = document.createElement("div")
+    likeButton.classList.add("like-button")
+    likeButton.textContent = "♥"
+    msgFooter.appendChild(likeButton)
+
+    let likeCounter = document.createElement("div")
+    likeCounter.classList.add("like-counter")
+    likeCounter.textContent = endorsementLikes
+    msgFooter.appendChild(likeCounter)
+
+    newMessage.appendChild(msgFooter)
     
-    newEl.textContent = `Sender: ${sender}, Receiver: ${receiver}, Message: ${textMsg}`
-    
-    messageBoard.append(newEl)
+    likeButton.addEventListener("click", function() {
+        if(localStorage.getItem(endorsementID)) {
+            console.log("already liked")
+            likeButton.classList.add("liked")
+        } else {
+            endorsement[1].likes++
+            update(ref(database, `endorsements/${endorsementID}`), endorsement[1])
+            localStorage.setItem(endorsementID, "true")
+            likeButton.classList.add("liked")
+        }
+    })
+
+    messageBoard.append(newMessage)
 }
